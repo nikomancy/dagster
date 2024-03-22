23b944637df2d3a3bf6863b923b5d5f2e256eb21
Sandy Ryza <sandy@elementl.com>
2024-03-22 16:57:05 -0700

    -> MetadataSet

--- python_modules/dagster/dagster_tests/definitions_tests/metadata_tests/test_table_metadata_mixin.py
+++ python_modules/dagster/dagster_tests/definitions_tests/metadata_tests/test_table_metadata_mixin.py
@@ -1,5 +1,5 @@
 from dagster import AssetKey, AssetMaterialization, TableColumn, TableSchema
-from dagster._core.definitions.metadata import TableMetadataMixin
+from dagster._core.definitions.metadata import TableMetadataSet
 from dagster._core.definitions.metadata.table import (
     TableColumnDep,
     TableColumnLineage,
@@ -8,7 +8,7 @@ from dagster._core.definitions.metadata.table import (
 
 def test_table_metadata_mixin():
     column_schema = TableSchema(columns=[TableColumn("foo", "str")])
-    table_metadata = TableMetadataMixin(column_schema=column_schema)
+    table_metadata = TableMetadataSet(column_schema=column_schema)
 
     dict_table_metadata = dict(table_metadata)
     assert dict_table_metadata == {"dagster/column_schema": column_schema}
@@ -20,8 +20,8 @@ def test_table_metadata_mixin():
     assert isinstance(splat_table_metadata["dagster/column_schema"], TableSchema)
     AssetMaterialization(asset_key="a", metadata=splat_table_metadata)
 
-    assert dict(TableMetadataMixin()) == {}
-    assert TableMetadataMixin.extract(dict(TableMetadataMixin())) == TableMetadataMixin()
+    assert dict(TableMetadataSet()) == {}
+    assert TableMetadataSet.extract(dict(TableMetadataSet())) == TableMetadataSet()
 
 
 def test_column_specs() -> None:
@@ -37,18 +37,18 @@ def test_column_specs() -> None:
     )
     expected_metadata = {"dagster/column_lineage": expected_column_lineage}
 
-    table_metadata = TableMetadataMixin(column_lineage=expected_column_lineage)
+    table_metadata = TableMetadataSet(column_lineage=expected_column_lineage)
 
     dict_table_metadata = dict(table_metadata)
     assert dict_table_metadata == expected_metadata
 
     materialization = AssetMaterialization(asset_key="foo", metadata=dict_table_metadata)
-    extracted_table_metadata_entries = TableMetadataMixin.extract(materialization.metadata)
-    assert extracted_table_metadata_entries.column_lineage == expected_column_lineage
+    extracted_table_metadata = TableMetadataSet.extract(materialization.metadata)
+    assert extracted_table_metadata.column_lineage == expected_column_lineage
 
     splat_table_metadata_entries = {**table_metadata}
     assert splat_table_metadata_entries == expected_metadata
 
     materialization = AssetMaterialization(asset_key="foo", metadata=splat_table_metadata_entries)
-    extracted_table_metadata_entries = TableMetadataMixin.extract(materialization.metadata)
-    assert extracted_table_metadata_entries.column_lineage == expected_column_lineage
+    extracted_table_metadata = TableMetadataSet.extract(materialization.metadata)
+    assert extracted_table_metadata.column_lineage == expected_column_lineage
